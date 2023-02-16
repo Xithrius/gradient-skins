10f4a1850d0ddac73a5bb8082c50a5a5168d9db6
Xithrius <xithrius@gmail.com>
2023-02-15 22:18:20 -0800

    The first gradient output, although it doesn't look too nice

--- gradient-skins/__main__.py
+++ gradient-skins/__main__.py
@@ -30,6 +30,36 @@ def sample_gradient(hs: tuple[bool, bool, bool]):
     return get_gradient_3d(64, 64, (8, 159, 143), (42, 72, 88), hs)
 
 
+removing = [
+    (0, 8, 0, 8),
+    (0, 8, 24, 64),
+    (8, 16, 32, 64),
+    (16, 20, 0, 4),
+    (16, 20, 12, 20),
+    (16, 20, 36, 44),
+    (16, 20, 52, 64),
+    (32, 64, 0, 16),
+    (32, 48, 16, 64),
+    (48, 64, 48, 64),
+    (48, 52, 44, 48),
+    (48, 52, 28, 36),
+    (48, 52, 16, 20)
+]
+
+
+def main():
+    array = sample_gradient((True, True, False))
+    img = Image.fromarray(np.uint8(array)).convert('RGBA')
+    img_arr = np.array(img)
+
+    for (y1, y2, x1, x2) in removing:
+        img_arr[y1:y2, x1:x2] = (0, 0, 0, 0)
+
+    img = Image.fromarray(img_arr)
+
+    img.save('examples/test.png', quality=100)
+
+
 def generator():
     array = sample_gradient((True, True, True))
     Image.fromarray(np.uint8(array)).save('examples/gradient_h.png', quality=100)
@@ -42,4 +72,5 @@ def generator():
 
 
 if __name__ == "__main__":
-    generator()
+    # generator()
+    main()
